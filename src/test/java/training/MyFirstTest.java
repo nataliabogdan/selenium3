1f6def4168964ff368c0a28e464f0346e25ee580
Natalia Bogdan <plopik.tala@gmail.com>
2018-10-23 20:49:36 +0200

    Updated My first test

--- src/test/java/training/MyFirstTest.java
+++ src/test/java/training/MyFirstTest.java
@@ -3,7 +3,6 @@ package training;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MyFirstTest extends TestBase {
@@ -11,14 +10,12 @@ public class MyFirstTest extends TestBase {
   @Test
   public void myFirstTest(){
     String value = "webdriver";
-    driver.navigate().to("https://search.maven.org/");
-    wait.until(ExpectedConditions.elementToBeClickable(By.id("mat-input-1"))).sendKeys(value + Keys.ENTER);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("search-result")));
-    String artifactId = driver.findElement(By.xpath("//td[contains(@class, 'cdk-column-artifactId')]//a")).getText();
+    driver.navigate().to("https://search.maven.org/classic/");
+    wait.until(ExpectedConditions.elementToBeClickable(By.id("query"))).sendKeys(value);
+    driver.findElement(By.id("queryButton")).click();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("resultTable")));
+    String artifactId = driver.findElement(By.xpath("//a[@id='artifactId']")).getText();
     Assert.assertEquals(artifactId, value);
-
-
-
 
   }
 }
