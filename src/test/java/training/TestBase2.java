104fdd9bbf60b74e1fa2255cccc3b9ce6020dcd8
Natalia Bogdan <plopik.tala@gmail.com>
2018-11-26 21:43:13 +0100

    Page Object. Step 6

---
+++ src/test/java/training/TestBase2.java
@@ -0,0 +1,26 @@
+package training;
+
+import org.junit.Before;
+
+public class TestBase2 {
+
+  public static ThreadLocal<Application> tlApp = new ThreadLocal<>();
+  public Application app;
+
+  @Before
+  public void start() {
+    if (tlApp.get() != null) {
+      app = tlApp.get();
+      return;
+    }
+
+    app = new Application();
+    tlApp.set(app);
+
+    Runtime.getRuntime().addShutdownHook(
+            new Thread(() -> {
+              app.quit();
+              app = null;
+            }));
+  }
+}
--- src/test/java/training/tests/PageObjectExampleTest.java
+++ src/test/java/training/tests/PageObjectExampleTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import training.Customer;
 import training.DataProviders;
+import training.TestBase2;
+
 import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
@@ -16,52 +18,19 @@ import java.util.Set;
 import static java.util.stream.Collectors.toSet;
 
 @RunWith(DataProviderRunner.class)
-public class PageObjectExampleTest extends TestBase {
+public class PageObjectExampleTest extends TestBase2 {
 
   @Test
   @UseDataProvider(value = "validCustomers", location = DataProviders.class)
   public void canRegisterCustomer(Customer customer) {
-    Set<String> oldIds = getCustomerIds();
-    registerNewCustomer(customer);
-    Set<String> newIds = getCustomerIds();
+    Set<String> oldIds = app.getCustomerIds();
+    app.registerNewCustomer(customer);
+    Set<String> newIds = app.getCustomerIds();
     assertTrue(newIds.containsAll(oldIds));
     assertTrue(newIds.size() == oldIds.size() + 1);
   }
 
-  private void registerNewCustomer(Customer customer) {
-    driver.get("http://localhost/litecart/en/create_account");
 
-    driver.findElement(By.name("firstname")).sendKeys(customer.getFirstname());
-    driver.findElement(By.name("lastname")).sendKeys(customer.getLastname());
-    driver.findElement(By.name("address1")).sendKeys(customer.getAddress());
-    driver.findElement(By.name("postcode")).sendKeys(customer.getPostcode());
-    driver.findElement(By.name("city")).sendKeys(customer.getCity());
 
-    driver.findElement(By.cssSelector("[id ^= select2-country_code]")).click();
-    driver.findElement(By.cssSelector(String.format(".select2-results__option[id $= %s", customer.getCountry()))).click();
-    wait.until((WebDriver d) -> d.findElement(By.cssSelector("select[name=zone_code] option[value=KS]")));
 
-    Select zoneselector = new Select(driver.findElement(By.xpath("//select[@name='zone_code']")));
-    zoneselector.selectByVisibleText(customer.getZone());
-
-    driver.findElement(By.name("email")).sendKeys(customer.getEmail());
-    driver.findElement(By.name("phone")).sendKeys(customer.getPhone());
-    driver.findElement(By.name("password")).sendKeys(customer.getPassword());
-    driver.findElement(By.name("confirmed_password")).sendKeys(customer.getPassword());
-    driver.findElement(By.name("create_account")).click();
-  }
-
-  private Set<String> getCustomerIds() {
-    driver.get("http://localhost/litecart/admin");
-    if (driver.findElements(By.id("box-login")).size() > 0) {
-      driver.findElement(By.name("username")).sendKeys("admin");
-      driver.findElement(By.name("password")).sendKeys("admin");
-      driver.findElement(By.name("login")).click();
-      wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
-    }
-    driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
-    return driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
-            .map(e -> e.findElements(By.tagName("td")).get(2).getText())
-            .collect(toSet());
-  }
 }
